69656620bdc395e56fb441c579b3b720e56e399f
Mira Keersmaekers <47260480+mirakeers@users.noreply.github.com>
2026-03-31 20:40:07 +0200

    feat(baseLayout): create base layout and first tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Card } from "./components/Card/Card";
+
 function App() {
   return (
     <>
-      <h1 className="text-violet-700">Personal Finance Tracker</h1>
+      <div className="flex flex-col px-8 md:px-32 max-w-7xl gap-12 mx-auto py-8 ">
+        <header>
+          <h1>Spending tracker</h1>
+        </header>
+        <main className="flex flex-col gap-4 md:gap-8 font-sans">
+          <h2 className="sr-only">Timeline</h2>
+          <Card>Timeline component</Card>
+          <h2>My transactions</h2>
+          <Card>DataGrid component</Card>
+        </main>
+      </div>
     </>
   );
 }
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,8 @@
+import type { ComponentProps } from "react";
+type CardProps = ComponentProps<"div">;
+
+export const Card = ({ className, ...props }: CardProps) => {
+  return <div className={`bg-b-card p-6 ${className}`} {...props} />;
+};
+
+<div className="bg-"></div>;
